d1ee65b9ad58abbead0b01422b35980e4269eacf
abhijeet verma <abhijeetverma155@gmail.com>
2023-12-07 14:28:07 +0530

    first commit

--- app.js
+++ app.js
@@ -2,5 +2,5 @@ const _ = require("lodash");
 
 const items = [1, [2, [3, [4]]]];
 
-const newitems = _.flattenDeep(items);
-console.log(newitems);
+const newItems = _.flattenDeep(items);
+console.log(newItems);
